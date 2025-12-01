c7399e991ad14a442758fea24f1d92303b42648d
Ankit Kumar Singh <ankitsingh13022003@gmail.com>
2025-12-01 19:44:56 +0530

    Add files via upload

---
+++ Single-Select-Accordion/src/components/accordian/data.js
@@ -0,0 +1,27 @@
+const data = [
+  {
+    id: '1',
+    question: "What is React?",
+    answer:
+      "React is a JavaScript library used for building fast and interactive user interfaces using a component-based architecture."
+  },
+  {
+    id: '2',
+    question: "What is the purpose of components in React?",
+    answer:
+      "Components allow developers to break the UI into reusable pieces, making the code modular, organized, and easier to maintain."
+  },
+  {
+    id: '3',
+    question: "What is the use of useState hook?",
+    answer:
+      "The useState hook allows React components to manage internal state, enabling dynamic updates and re-rendering when the state changes."
+  },
+  {
+    id: '4',
+    question: "What is the Virtual DOM?",
+    answer:
+      "The Virtual DOM is a lightweight copy of the real DOM that React uses to optimize updates by calculating the most efficient way to apply changes."
+  }
+];
+export default data;
